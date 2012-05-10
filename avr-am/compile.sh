d295148ea570ae40f2eb3bdb4718e30837ee11d3
João Paulo Pizani Flor <joaopizani@gmail.com>
2012-05-10 17:42:22 -0300

    compile script will be substituted for a nicer standardized Makefile

--- avr-am/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-OPTS="-I./include -I../avrutils/include -mmcu=atmega168p -O1 -fpack-struct -fshort-enums -funsigned-bitfields -funsigned-char -Wall -Wstrict-prototypes -DF_CPU=16000000UL -c"
-
-avr-gcc $OPTS src/avr-am/config.c
-avr-gcc $OPTS src/avr-am/toggle.c
