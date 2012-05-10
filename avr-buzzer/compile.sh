90d366b7654a537e9b03c65b23e875b51a377d62
João Paulo Pizani Flor <joaopizani@gmail.com>
2012-05-10 17:51:50 -0300

    compile script and binary lib file won't be needed anymore in avr-buzzer 'cause now we are going to use a nice standardized Makefile able to build libs trivially

--- avr-buzzer/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-OPTS="-mmcu=atmega168p -O2 -fpack-struct -fshort-enums -funsigned-bitfields -funsigned-char -Wall -Wstrict-prototypes -DF_CPU=16000000UL -c"
-
-avr-gcc $OPTS src/toggle.c
