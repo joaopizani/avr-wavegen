b7d7047cee3da4f140980e6991d515432c8ca66a
João Paulo Pizani Flor <joaopizani@gmail.com>
2012-05-10 11:35:25 -0300

    removed avrutils from this repo as it will now be a repo (and lib) of its own (now avr-utils)

--- avrutils/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-OPTS="-I./include -mmcu=atmega168p -O1 -fpack-struct -fshort-enums -funsigned-bitfields -funsigned-char -Wall -Wstrict-prototypes -DF_CPU=16000000UL -c"
-
-avr-gcc $OPTS src/avrutils/timers-atmega168.c
-avr-gcc $OPTS src/avrutils/timers-atmega168p.c
