98d49af48c3ea05c96bf351bf04585116e123603
João Paulo Pizani Flor <joaopizani@gmail.com>
2012-05-14 17:03:39 -0300

    altered avr-tone to use the alarm library for asynchronous handling of tone duration. Added the avr-alarm dependency to paths.def accordingly

--- avr-tone/src/avr-tone.c
+++ avr-tone/src/avr-tone.c
@@ -1,6 +1,8 @@
 #include <avr/io.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <timers-atmega168p.h>
+#include <alarm.h>
 #include "avr-tone.h"
 
 
@@ -8,10 +10,12 @@ void initOutput(void) {
     DDRB |= _BV(PB0);
 }
 
+
 void initToneTimer(void) {
     TCCR2A |= _BV(WGM21); // clear on compare match
     TIMSK2 |= _BV(OCIE2A); // enable compare match intr.
 }
+
 
 inline void startToneTimer(void) {
     TCCR2B |= getTimer2PrescaleBits_atmega168p(TONE_PRESCALE);
@@ -22,30 +26,41 @@ inline void stopToneTimer(void) {
     PORTB = 0xff;
 }
 
+inline void quiet(void) {
+    OCR2A = 0x00;
+    TCNT2 = 0x00;
+}
+
 void setToneFreq(uint16_t f_Hz) {
-    if(f_Hz == 0) { // silence, so we stop vibrating
-        OCR2A = 0x00;
-        TCNT2 = 0x00;
+    const uint32_t toneTimerPeriodCount = TONE_TIMER_FREQ_HZ / f_Hz;
+    const uint32_t toneTimerToggleCount = toneTimerPeriodCount / 2;
+    OCR2A = toneTimerToggleCount;
+}
+
+void setFreqAlarmHandler(void* freq_uint16_ptr) {
+    uint16_t* f_Hz_ptr = (uint16_t*) freq_uint16_ptr;
+    uint16_t f_Hz = *f_Hz_ptr; // saving
+    free(f_Hz_ptr); // we don't need it anymore
+
+    if(f_Hz == 0) { // silence
+        quiet();
         stopToneTimer();
-    } else {
-        const uint32_t toneTimerPeriodCount = TONE_TIMER_FREQ_HZ / f_Hz;
-        const uint32_t toneTimerToggleCount = toneTimerPeriodCount / 2;
-        OCR2A = toneTimerToggleCount;
+    } else { // sound
+        setToneFreq(f_Hz);
+        startToneTimer();
     }
 }
 
-/**
- * USE AVR-ALARM TO IMPLEMENT DURATION IN AN ASYNCH FASHION.
- */
 
 void playTone(uint16_t f_Hz, uint16_t dur_ms) {
-    long i; //TODO implement FOR REALZ using 16-bit counter
+    static uint16_t zero_Hz = 0;
+    static uint16_t* zero_Hz_ptr = &zero_Hz;
 
-    if(dur_ms != 0) { // if duration is zero do nothing
-        setToneFreq(f_Hz);
-        startToneTimer();
-        for(i = 0; i < 0x001fffff; i++); //TODO implement FOR REALZ using 16-bit cnt.
-        stopToneTimer();
-    }
+    uint16_t* f_Hz_ptr = malloc(sizeof(uint16_t));
+    *f_Hz_ptr = f_Hz;
+
+    time_ms_t t = alarm_until_last_deadline();
+    alarm_insert(t, setFreqAlarmHandler, f_Hz_ptr); // beginning of the sound
+    alarm_insert(t + dur_ms, setFreqAlarmHandler, zero_Hz_ptr); // silence thereafter
 }
 
--- avr-tone/src/avr-tone.h
+++ avr-tone/src/avr-tone.h
@@ -2,6 +2,7 @@
 #define AVRTONE_H
 
 #include <stdint.h>
+#include <alarm.h>
 
 #define TONE_PRESCALE 1024
 #define TONE_TIMER_FREQ_HZ (F_CPU / TONE_PRESCALE)
@@ -13,8 +14,12 @@ void initToneTimer(void);
 inline void startToneTimer(void);
 inline void stopToneTimer(void);
 
+inline void quiet(void);
 void setToneFreq(uint16_t f_Hz);
-void playTone(uint16_t f_Hz, uint16_t dur_ms);
+
+void setFreqAlarmHandler(void* freq_uint16_ptr);
+
+void playTone(uint16_t f_Hz, time_ms_t dur_ms);
 
 
 #endif /* AVRTONE_H */
