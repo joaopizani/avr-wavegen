36454d8d1feeb97d6ca720b2dd4cd4f28701bcd0
João Paulo Pizani Flor <joaopizani@gmail.com>
2012-05-15 17:13:05 -0300

    Makefiles and respective files modified to use atmega168 (not p) as MCU. I promise this is THE LAST TIME I commit such changes to such a trivial variable. The only reason why I chose to leave MCU as atmega168 is that the original Arduino Duemilanove uses this MCU. My "chinese" version uses atmega168p, however.

--- avr-tone/Makefile
+++ avr-tone/Makefile
@@ -39,7 +39,7 @@
 # Name of target controller 
 # (e.g. 'at90s8515', see the available avr-gcc mmcu 
 # options for possible values)
-MCU=atmega168p
+MCU=atmega168
 
 # id to use with programmer
 # default: PROGRAMMER_MCU=$(MCU)
--- avr-tone/src/avr-tone.c
+++ avr-tone/src/avr-tone.c
@@ -1,7 +1,7 @@
 #include <avr/io.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <timers-atmega168p.h>
+#include <timers-atmega168.h>
 #include <alarm.h>
 #include "avr-tone.h"
 
@@ -21,7 +21,8 @@ void initToneTimer(void) {
 
 
 inline void startToneTimer(void) {
-    TCCR2B |= getTimer2PrescaleBits_atmega168p(TONE_PRESCALE);
+    TCNT2 = 0x00;
+    TCCR2B |= getTimer2PrescaleBits_atmega168(TONE_PRESCALE);
 }
 
 inline void stopToneTimer(void) {
