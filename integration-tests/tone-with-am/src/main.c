028c30391ce6814d6f8c90831b278ebc704a2eff
João Paulo Pizani Flor <joaopizani@gmail.com>
2012-06-23 13:55:29 -0300

    Changed the demo song of integration-tests/tone-with-am to the National Anthem of the Netherlands :)

--- integration-tests/tone-with-am/src/main.c
+++ integration-tests/tone-with-am/src/main.c
@@ -2,12 +2,26 @@
 #include <avr-am/config.h>
 #include <avr-tone/avr-tone.h>
 
-#include <util/delay.h>
+
+/*
+ * Test program for the avr-tone library using AM as backend.
+ */
+
+// By using the SHORT_BEEPS define, we can have shorter tones for easier simulation
+#define BEEP_T 10
+#ifdef SHORT_BEEPS
+#undef BEEP_T
+#define BEEP_T 1
+#endif
+
+
+// frequencies and durations used in the demo song
+#define PA 0 //Hz
 
 #define C4 261 //Hz
 #define D4 293 //Hz
 #define E4 329 //Hz
-#define F4 349 //Hz
+#define F4_ 369 //Hz
 #define G4 391 //Hz
 #define A4 440 //Hz
 #define B4 493 //Hz
@@ -15,27 +29,17 @@
 #define C5 523 //Hz
 #define D5 587 //Hz
 #define E5 659 //Hz
-#define F5 698 //Hz
+#define F5_ 739 //Hz
 #define G5 783 //Hz
 #define A5 880 //Hz
 #define B5 987 //Hz
 
 #define C6 1046 //Hz
 
+#define QNOTE (80 * BEEP_T) //ms
+#define ENOTE (40 * BEEP_T) //ms
+#define HNOTE (160 * BEEP_T) //ms
 
-/*
- * Test program for the avr-tone library using AM as backend.
- * Synchronization between producer of tones (user thread) and consumer
- * (interrupt context) is - as of now - done manually. That's the reason
- * for the _delay_ms(t) you see in the code...
- */
-
-// By using the SHORT_BEEPS define, we can have shorter tones for easier simulation
-#define BEEP_T 200
-#ifdef SHORT_BEEPS
-#undef BEEP_T
-#define BEEP_T 50
-#endif
 
 int main(void) {
     initOutput();
@@ -43,24 +47,29 @@ int main(void) {
     initCarrierTimer();
     setCarrierFreqKHz(560);
 
-    // long "beep" so that we can notice the scale coming :)
-    playTone(1000, (BEEP_T * 2));
-    playTone(0, (BEEP_T * 2));
-
     sei();
 
-    // C major scale, to test that the tones are played in tune :)
+    // Het Wilhelmus, National Anthem of The Netherlands :)
     while(1) {
-        playTone(C4, BEEP_T); playTone(D4, BEEP_T); playTone(E4, BEEP_T); playTone(F4, BEEP_T);
-        playTone(G4, BEEP_T); playTone(A4, BEEP_T); playTone(B4, BEEP_T); playTone(C5, BEEP_T);
-        playTone(C5, BEEP_T); playTone(B4, BEEP_T); playTone(A4, BEEP_T); playTone(G4, BEEP_T);
-        playTone(F4, BEEP_T); playTone(E4, BEEP_T); playTone(D4, BEEP_T); playTone(C4, BEEP_T);
-        playTone(0, (BEEP_T * 2));
-        playTone(C5, BEEP_T); playTone(D5, BEEP_T); playTone(E5, BEEP_T); playTone(F5, BEEP_T);
-        playTone(G5, BEEP_T); playTone(A5, BEEP_T); playTone(B5, BEEP_T); playTone(C6, BEEP_T);
-        playTone(C6, BEEP_T); playTone(B5, BEEP_T); playTone(A5, BEEP_T); playTone(G5, BEEP_T);
-        playTone(F5, BEEP_T); playTone(E5, BEEP_T); playTone(D5, BEEP_T); playTone(C5, BEEP_T);
-        playTone(0, (BEEP_T * 2));
+        playTone(D4, QNOTE); playTone(G4, QNOTE); playTone(G4, QNOTE); playTone(A4, ENOTE); playTone(B4, ENOTE);
+        playTone(C5, ENOTE); playTone(A4, ENOTE); playTone(B4, QNOTE); playTone(A4, ENOTE); playTone(B4, ENOTE);
+        playTone(C5, QNOTE); playTone(B4, QNOTE); playTone(A4, ENOTE); playTone(G4, ENOTE); playTone(A4, QNOTE);
+        playTone(G4, HNOTE); playTone(PA, QNOTE);
+
+        playTone(D4, QNOTE); playTone(G4, QNOTE); playTone(G4, QNOTE); playTone(A4, ENOTE); playTone(B4, ENOTE);
+        playTone(C5, ENOTE); playTone(A4, ENOTE); playTone(B4, QNOTE); playTone(A4, ENOTE); playTone(B4, ENOTE);
+        playTone(C5, QNOTE); playTone(B4, QNOTE); playTone(A4, ENOTE); playTone(G4, ENOTE); playTone(A4, QNOTE);
+        playTone(G4, HNOTE); playTone(PA, QNOTE);
+
+        playTone(B4, ENOTE); playTone(C5, ENOTE); playTone(D5, HNOTE); playTone(E5, QNOTE); playTone(D5, HNOTE);
+        playTone(C5, QNOTE); playTone(B4, QNOTE); playTone(A4, ENOTE); playTone(B4, ENOTE); playTone(C5, QNOTE);
+        playTone(B4, QNOTE); playTone(A4, QNOTE); playTone(G4, QNOTE); playTone(A4, HNOTE); playTone(PA, QNOTE);
+
+        playTone(D4, QNOTE); playTone(G4, ENOTE); playTone(F4_, ENOTE); playTone(G4, ENOTE);
+        playTone(A4, ENOTE); playTone(B4, QNOTE); playTone(A4, HNOTE); playTone(G4, QNOTE);
+        playTone(F4_, QNOTE); playTone(D4, QNOTE); playTone(E4, ENOTE); playTone(F4_, ENOTE);
+        playTone(G4, QNOTE); playTone(G4, QNOTE); playTone(F4_, QNOTE); playTone(G4, HNOTE);
+        playTone(PA, HNOTE);
     }
 
     return 0;
